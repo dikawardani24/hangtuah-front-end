c892cf50a1b385180a75c360af4a5c7c812ce768
Dika Wardani <dikawardani24@gmail.com>
2020-07-13 04:27:02 +0700

    Finishing layout perwakilan & cabang

--- src/app/views/pages/cabang/cabang.component.ts
+++ src/app/views/pages/cabang/cabang.component.ts
@@ -4,6 +4,8 @@ import { DataPoolCabang } from 'src/app/core/generator/data-pool/cabang-data-poo
 import { DataInstansi } from 'src/app/core/generator/hangtuah/data-hangtuah';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { WidgetDetailCabangComponent } from '../../partials/content/cabang/widget-detail-cabang/widget-detail-cabang.component';
+import { TotalType } from 'src/app/core/generator/hangtuah-org-generator';
+import { SchoolType } from 'src/app/core/_base/crud/models/school-organization';
 
 @Component({
   selector: 'kt-cabang',
@@ -12,12 +14,73 @@ import { WidgetDetailCabangComponent } from '../../partials/content/cabang/widge
 })
 export class CabangComponent implements OnInit {
   listDataPool: DataInstansi<Cabang>[] = []
+  listTotalAll: TotalType[] = []
+  totalAll = 0
 
   constructor(
     private cabangDataPool: DataPoolCabang
   ) { }
 
+  private collectDataSummary() {
+    let totalTk = 0
+    let totalPaud = 0
+    let totalSD = 0
+    let totalSMP = 0
+    let totalSMA = 0
+    let totalSMK = 0
+
+    this.listDataPool.forEach(e => {
+      e.getTotalTypes().forEach(t => {
+        this.totalAll += t.total
+
+        if (t.type === SchoolType.PAUD) {
+          totalPaud += t.total
+        }
+
+        if (t.type === SchoolType.TK) {
+          totalTk += t.total
+        }
+
+        if (t.type === SchoolType.SD) {
+          totalSD += t.total
+        }
+
+        if (t.type === SchoolType.SMP) {
+          totalSMP += t.total
+        }
+
+        if (t.type === SchoolType.SMA) {
+          totalSMA += t.total
+        }
+
+        if (t.type === SchoolType.SMK) {
+          totalSMK += t.total
+        }
+      })
+    })
+
+    this.listTotalAll.push({
+      total: totalPaud, type: SchoolType.PAUD
+    })
+    this.listTotalAll.push({
+      total: totalTk, type: SchoolType.TK
+    })
+    this.listTotalAll.push({
+      total: totalSD, type: SchoolType.SD
+    })
+    this.listTotalAll.push({
+      total: totalSMP, type: SchoolType.SMP
+    })
+    this.listTotalAll.push({
+      total: totalSMA, type: SchoolType.SMA
+    })
+    this.listTotalAll.push({
+      total: totalSMK, type: SchoolType.SMK
+    })
+  }
+
   ngOnInit() {
     this.listDataPool = this.cabangDataPool.getListData()
+    this.collectDataSummary()
   }
 }
--- src/app/views/pages/perwakilan/perwakilan.component.ts
+++ src/app/views/pages/perwakilan/perwakilan.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DataInstansi } from 'src/app/core/generator/hangtuah/data-hangtuah';
 import { Perwakilan } from 'src/app/core/_base/crud/models/hangtuah-organization';
 import { DataPoolPerwakilan } from 'src/app/core/generator/data-pool/perwakilan-data-pool';
+import { TotalType } from 'src/app/core/generator/hangtuah-org-generator';
+import { SchoolType } from 'src/app/core/_base/crud/models/school-organization';
 
 @Component({
   selector: 'kt-perwakilan',
@@ -10,12 +12,73 @@ import { DataPoolPerwakilan } from 'src/app/core/generator/data-pool/perwakilan-
 })
 export class PerwakilanComponent implements OnInit {
   listDataPool: DataInstansi<Perwakilan>[] = []
+  listTotalAll: TotalType[] = []
+  totalAll = 0
 
   constructor(
     private perwakilanDataPool: DataPoolPerwakilan
   ) { }
 
+  private collectDataSummary() {
+    let totalTk = 0
+    let totalPaud = 0
+    let totalSD = 0
+    let totalSMP = 0
+    let totalSMA = 0
+    let totalSMK = 0
+
+    this.listDataPool.forEach(e => {
+      e.getTotalTypes().forEach(t => {
+        this.totalAll += t.total
+
+        if (t.type === SchoolType.PAUD) {
+          totalPaud += t.total
+        }
+
+        if (t.type === SchoolType.TK) {
+          totalTk += t.total
+        }
+
+        if (t.type === SchoolType.SD) {
+          totalSD += t.total
+        }
+
+        if (t.type === SchoolType.SMP) {
+          totalSMP += t.total
+        }
+
+        if (t.type === SchoolType.SMA) {
+          totalSMA += t.total
+        }
+
+        if (t.type === SchoolType.SMK) {
+          totalSMK += t.total
+        }
+      })
+    })
+
+    this.listTotalAll.push({
+      total: totalPaud, type: SchoolType.PAUD
+    })
+    this.listTotalAll.push({
+      total: totalTk, type: SchoolType.TK
+    })
+    this.listTotalAll.push({
+      total: totalSD, type: SchoolType.SD
+    })
+    this.listTotalAll.push({
+      total: totalSMP, type: SchoolType.SMP
+    })
+    this.listTotalAll.push({
+      total: totalSMA, type: SchoolType.SMA
+    })
+    this.listTotalAll.push({
+      total: totalSMK, type: SchoolType.SMK
+    })
+  }
+
   ngOnInit() {
     this.listDataPool = this.perwakilanDataPool.getListData()
+    this.collectDataSummary()
   }
 }
